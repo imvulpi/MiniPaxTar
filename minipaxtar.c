398dee27644e9a81775cdcef62cd3176e2246f8c
Vulpi <78976654+imvulpi@users.noreply.github.com>
2026-07-08 09:20:55 +0200

    feat: add function definitions for writing tars

---
+++ minipaxtar.c
@@ -0,0 +1,16 @@
+#include "minipaxtar.h"
+
+int mptar_write_header(mptar_write_ctx *write_ctx, const mptar_metadata *meta)
+{
+    return 0;
+}
+
+int mptar_write_data_chunk(mptar_write_ctx *write_ctx, const void *buffer, mptar_size_t size)
+{
+    return 0;
+}
+
+int mptar_write_finalize(mptar_write_ctx *write_ctx, const mptar_metadata *meta)
+{
+    return 0;
+}
--- minipaxtar.h
+++ minipaxtar.h
@@ -1,3 +1,57 @@
+#ifdef MPTAR_NO_STD
+    typedef unsigned char mptar_uint8;
+    
+    #if defined(__INT_MAX__) && (__INT_MAX__ == 32767)
+        typedef unsigned int mptar_uint16;
+    #else
+        typedef unsigned short mptar_uint16;
+    #endif
+
+    #if defined(__INT_MAX__) && (__INT_MAX__ == 2147483647)
+        typedef unsigned int mptar_uint32;
+    #else
+        typedef unsigned long mptar_uint32;
+    #endif
+
+    typedef unsigned long long mptar_uint64;
+
+    #if defined(__SIZEOF_POINTER__) && (__SIZEOF_POINTER__ == 8)
+        typedef unsigned long long mptar_size_t;
+    #else
+        typedef unsigned long mptar_size_t;
+    #endif
+
+    #define MPTAR_NULL ((void*)0)
+#else
+    #include <stdint.h>
+    #include <stddef.h>
+    
+    typedef uint64_t mptar_uint64;
+    typedef uint32_t mptar_uint32;
+    typedef uint16_t mptar_uint16;
+    typedef uint8_t  mptar_uint8;
+    typedef size_t   mptar_size_t;
+    
+    #define MPTAR_NULL NULL
+#endif
+
+typedef struct {
+    const char* path;     // Unlimited length in memory
+    mptar_uint64 size;        // True 64-bit size
+    mptar_uint32 mode;        // Octal permissions
+    mptar_uint32 uid;         // Numeric Owner ID
+    mptar_uint32 gid;         // Numeric Group ID
+    mptar_uint64 mtime;       // Modification Timestamp
+    char typeflag;        // '0' = file, '5' = directory
+} mptar_metadata;
+
+typedef mptar_size_t (*mptar_write_fn)(void* user_data, const void* buffer, mptar_size_t size);
+typedef struct {
+    mptar_write_fn write_fn;
+    void* user_data;
+    mptar_uint64 bytes_left;
+} mptar_write_ctx;
+
 // STANDARD HEADER:
 // Octal strings have to be null/space terminated.
 typedef struct {
@@ -26,3 +80,7 @@ typedef struct {
 // Line Length + " " + KEYWORD + "=" + VALUE + "\n"
 // Where:
 //  Line length is the length of the line including the length of the characters defining the length.
+
+int mptar_write_header(mptar_write_ctx* write_ctx, const mptar_metadata* meta);
+int mptar_write_data_chunk(mptar_write_ctx* write_ctx, const void* buffer, mptar_size_t size);
+int mptar_write_finalize(mptar_write_ctx* write_ctx, const mptar_metadata* meta);
