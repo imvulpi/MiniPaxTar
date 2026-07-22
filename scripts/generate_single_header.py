a800599fcd459fec6227b318c963ce2055d9057a
Vulpi <78976654+imvulpi@users.noreply.github.com>
2026-07-22 04:35:58 +0200

    feat: check for missing headers when generating files.

--- scripts/generate_single_header.py
+++ scripts/generate_single_header.py
@@ -1,11 +1,16 @@
 from pathlib import Path
 
-ROOT = Path(__file__).parent
-HEADER_PATH = ROOT / ".." / "minipaxtar.h"
-SRC_PATH = ROOT / ".." / "minipaxtar.c"
-OUTPUT_PATH = ROOT / ".." / "dist" / "minipaxtar.h"
+SCRIPT_DIR = Path(__file__).resolve().parent
+ROOT = SCRIPT_DIR.parent
+
+HEADER_PATH = ROOT / "minipaxtar.h"
+SRC_PATH = ROOT / "minipaxtar.c"
+OUTPUT_PATH = ROOT / "dist" / "minipaxtar.h"
 
 def generate_single_header():
+    if not HEADER_PATH.exists() or not SRC_PATH.exists():
+        raise FileNotFoundError("Source files missing!")
+    
     header_code = HEADER_PATH.read_text(encoding="utf-8")
     src_code = SRC_PATH.read_text(encoding="utf-8")
 
